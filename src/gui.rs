c59ee9d30b6b5a867ff0f2aa9ba0d9b1fa4ca860
LarryHsiao <larryhsiao@larryhsiao.com>
2019-11-22 01:05:36 +0800

    fix for windows

--- src/gui.rs
+++ src/gui.rs
@@ -7,7 +7,10 @@ pub struct UI {}
 impl Action for UI {
     fn fire(&self) -> Result<(), Box<dyn Error>> {
         let mut frame = sciter::Window::new();
-        frame.load_file("html/index.html");
+        let mut path = std::env::current_dir()?;
+        path.push("html");
+        path.push("index.html");
+        frame.load_file(format!("file://{}", path.to_str().unwrap()).as_str());
         frame.run_app();
         Ok(())
     }
