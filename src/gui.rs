25970265c5b211e4be890dc25d49f306e7a19206
LarryHsiao <larryhsiao@larryhsiao.com>
2019-11-25 16:29:57 +0800

    enable inspector in debug build

--- src/gui.rs
+++ src/gui.rs
@@ -1,5 +1,7 @@
 use crate::arch::Action;
 use std::error::Error;
+use sciter::types::SCITER_RT_OPTIONS::SCITER_SET_DEBUG_MODE;
+use sciter::window::Options::{DebugMode, AlphaWindow, TransparentWindow};
 
 /// The terminal UI
 pub struct UI {}
@@ -7,6 +9,7 @@ pub struct UI {}
 impl Action for UI {
     fn fire(&self) -> Result<(), Box<dyn Error>> {
         let mut frame = sciter::Window::new();
+        frame.set_options(DebugMode(cfg!(debug_assertions)));
         let mut path = std::env::current_dir()?;
         path.push("html");
         path.push("index.html");
